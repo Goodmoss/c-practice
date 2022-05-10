a2c8c77098413b819199023358c170a924c0b7a2
moss <eki01771@gmail.com>
2022-05-10 09:49:52 +0900

    함수 : 속도

---
+++ 제어문/4-03_speed.c
@@ -0,0 +1,28 @@
+// 함수(3)
+// 속도 출력 문제
+
+#include <stdio.h>
+
+int speed = 0;
+
+int speedChange();
+
+int main()
+{
+	
+
+	while (1)
+	{
+		scanf_s("%d", &speed);
+		printf("현재 속도: %d\n", speedChange(speed));
+	}
+
+	return 0; 
+}
+
+int speedChange(int inc)	// 속도 변경 함수
+{
+	int i = 0;
+	inc = ;
+	return inc;
+}
